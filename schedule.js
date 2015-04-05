edd55b1fb2b39587646150f2cda8da88c8aa9761
Paul Fitzgerald <paulfitz99@gmail.com>
2015-04-05 16:59:26 +0100

    added start end time and screenshot

--- schedule.js
+++ schedule.js
@@ -7,6 +7,27 @@ $(document).on('click', '#genres li', function(){
   getTomorrowsSchedule(genre);
 });
 
+function formatDate(start, end) {
+  start_date = new Date(start);
+  end_date = new Date(end);
+
+  day = start_date.getDate();
+  month = start_date.getMonth() + 1;
+  year = start_date.getFullYear();
+
+  start_hour = start_date.getHours();
+  start_mins = start_date.getMinutes();
+
+  end_hour = end_date.getHours();
+  end_mins = end_date.getMinutes();
+
+  date = day + "/" + month + "/" + year + " ";
+  date +=  ("0"+start_hour).slice(-2) + ":" + ("0"+start_mins).slice(-2) + " - " +
+           ('0' + end_hour).slice(-2) + ":" +  ( "0" + end_mins).slice(-2);
+  return date;
+}
+
+
 function getGenres (){
  $.ajax({
   url: "http://www.bbc.co.uk/tv/programmes/genres.json",
@@ -36,8 +57,10 @@ function getTomorrowsSchedule(genre){
       var synopsis = data.broadcasts[i].programme.short_synopsis;
       var image = data.broadcasts[i].programme.image.pid;
       var duration = data.broadcasts[i].duration;
+      var start = data.broadcasts[i].start;
+      var end = data.broadcasts[i].end;
       var minutes = Math.floor(duration / 60);
-      $('#programmes').append("<li><h2>" + deets + "</h2>" + synopsis + "<br>" + " " + minutes + " mins" + "<br>" + "<img src =http://ichef.bbci.co.uk/images/ic/272x153/" + image + ".jpg >");
+      $('#programmes').append("<li><h2>" + deets + "</h2>" + "<p id='synopsis'>" +  "<img src =http://ichef.bbci.co.uk/images/ic/272x153/" + image + ".jpg >" + "<br>" + synopsis + "</p>"+ "<p id='time'>" + formatDate(start, end));
     };
   });
 };
